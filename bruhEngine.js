1276df930b276f28b3de50070d082ab929e0350b
Brian Longmore <43781990+branogarbo@users.noreply.github.com>
2020-01-16 11:27:02 -1000

    Pull from git when you get to yo computer.

--- bruhEngine.js
+++ bruhEngine.js
@@ -1,6 +1,11 @@
-document.onkeypress = event=>{
+document.onkeypress = event => {
   if (event.key == "b") {
     var bruhAudio = new Audio('bruh-sound-effect.mp3');
     bruhAudio.play();
   }
-}
+};
+
+document.onclick = event => {
+  var bruhAudio = new Audio('bruh-sound-effect.mp3');
+  bruhAudio.play();
+};
